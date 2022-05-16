66f522b642383d1743a0e446004b52b2bd9c9601
bayoog <obaydaba96@gmail.com>
2022-05-16 17:29:15 +0200

    feature: add product model

---
+++ models/example/product.sql
@@ -0,0 +1,48 @@
+
+{{ 
+    config(
+        alias=env_var('DESTINATION_TABLE', var('destination_table', ''))
+    )
+ }}
+
+with temp_table as (
+    select
+        seller_sku as SKU,
+        asin1 as ASIN,
+        item_name as "item name",
+        item_description as "item description",
+        status as "status",
+        merchant_shipping_group as "merchant shipping group",
+        item_condition as "item condition",
+        cd.quantity as "sellable inventory",
+        cd.country as "marketplace",
+        cd."fulfillment-center-id" as "fulfillment"
+        -- cd.fulfillment as "fulfillment"
+        
+    from  {{ 
+        source(
+            env_var('DATABASE_SCHEMA', var('source_schema', '')),
+            env_var('SOURCE_MERCHANT_LISTING_ALL_DATA', var('source_table', ''))
+        ) 
+    }}  mlad
+    inner join {{
+        source(
+            env_var('DATABASE_SCHEMA', var('source_schema', '')),
+            env_var('SOURCE_FBA_FULFILLMENT_CURRENT_INVENTORY_REPORT', var('source_table', ''))
+        )
+    }} cd on mlad.seller_sku = cd.sku 
+)
+
+select 
+    SKU,
+    ASIN,
+    "item name",
+    "item description",
+    "status",
+    "merchant shipping group",
+    "item condition",
+    "sellable inventory",
+    "marketplace",
+    "fulfillment"
+    
+    from temp_table
